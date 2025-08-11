6af0259a73b6b6afa95b14433d562978972bca8f
GAMANASREE8985 <163974739+GAMANASREE8985@users.noreply.github.com>
2025-08-11 12:13:36 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const app = express();
 
+// Define root route
 app.get('/', (req, res) => {
-  res.send('Hello, Automated Deployment!');
+  res.send('Hello');
 });
 
-app.listen(3000, () => {
-  console.log('App running on http://localhost:3000');
-});
+// Only start the server if this file is run directly (not during testing)
+if (require.main === module) {
+  app.listen(3000, () => {
+    console.log('App running on http://localhost:3000');
+  });
+}
 
-module.exports = app;  // Export for testing
+module.exports = app;
